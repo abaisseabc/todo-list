4d2641c07f762aee43dde651e79a344229cf0d3d
abaisseabc <96632270+abaisseabc@users.noreply.github.com>
2022-10-31 18:04:31 +0300

    добавлен функционал для сохранения состояния state в LocalStorage

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,14 @@
 import { createStore } from 'vuex'
 
+let post = window.localStorage.getItem('post');
+let task = window.localStorage.getItem('task');
+let untask = window.localStorage.getItem('untask')
+
 export default createStore({
   state: {
-    posts: [],
-    completed_tasks: [],
-    unfinished_tasks: []
+    posts: post ? JSON.parse(post) : [],
+    completed_tasks: task ? JSON.parse(task) : [],
+    unfinished_tasks: untask ? JSON.parse(untask) : []
   },
   getters: {
     POSTS(state) {
@@ -20,32 +24,47 @@ export default createStore({
   mutations: {
     PUSH_POST_TO_ARRAY_POSTS: (state, post) => {
       state.posts.push(post);
+      window.localStorage.setItem('post', JSON.stringify(state.posts));
     },
     DELETE_POST_FROM_ARRAY_POSTS: (state, post) => {
       let filterPosts = state.posts.filter(item => {return item.id != post.id});
       state.posts = filterPosts;
+
+      window.localStorage.setItem('post', JSON.stringify(state.posts));
     },
     ADD_COMPLETE_TASKS_TO_ARRAY_COMPLETED_TASKS: (state, task) => {
       let filterPosts = state.posts.filter(item => {return item.id != task.id});
       state.posts = filterPosts;
+      window.localStorage.setItem('post', JSON.stringify(state.posts));
 
       state.completed_tasks.push(task);
+      window.localStorage.setItem('task', JSON.stringify(state.completed_tasks));
+
 
       let filteredUnfinishedTasks = state.unfinished_tasks.filter(item => {return item.id != task.id});
       state.unfinished_tasks = filteredUnfinishedTasks;
+      window.localStorage.setItem('untask', JSON.stringify(state.unfinished_tasks));
     },
     ADD_UNFINISHED_TASK_TO_ARRAY_UNFINISHED_TASKS: (state, unTask) => {
+
       let filterPosts = state.posts.filter(item => {return item.id != unTask.id});
       state.posts = filterPosts;
+
+      window.localStorage.setItem('post', JSON.stringify(state.posts));
+
+
       state.unfinished_tasks.push(unTask);
+      window.localStorage.setItem('untask', JSON.stringify(state.unfinished_tasks));
     },
     DELETE_TASK_IN_ARRAY_COMPLETED_TASKS: (state, task) => {
       let filterPosts = state.completed_tasks.filter(item => {return item.id != task.id});
       state.completed_tasks = filterPosts;
+      window.localStorage.setItem('task', JSON.stringify(state.completed_tasks));
     },
     DELETE_UNTASK_IN_ARRAY_UNFINISHED_TASKS: (state, unTask) => {
       let filterPosts = state.unfinished_tasks.filter(item => {return item.id != unTask.id});
       state.unfinished_tasks = filterPosts;
+      window.localStorage.setItem('untask', JSON.stringify(state.unfinished_tasks));
     }
   },
   actions: {
